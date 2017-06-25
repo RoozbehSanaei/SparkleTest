4047945c3b092ebcdb6fe8a1c2916326af54bb3f
roozbehsanaei <roozbeh.sanaei2@gmail.com>
2017-06-25 18:49:51 +0700

    / ‘examples/java/JNI/simple/test.sh’

--- examples/java/JNI/simple/test.sh
+++ examples/java/JNI/simple/test.sh
@@ -2,8 +2,8 @@ set -o xtrace
 mkdir build
 jni_path=/usr/lib/jvm/java-8-oracle/include
 jni_md_path=/usr/lib/jvm/java-8-oracle/include/linux
-javac HelloJNI.java
-javah HelloJNI
+javac build/HelloJNI.java
+javah build/HelloJNI
 gcc -I $jni_path -I $jni_md_path HelloJNI.c -shared -o hello.so
 java HelloJNI
 rmdir build
