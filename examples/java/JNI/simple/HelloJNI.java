22bb709b31d8a41c03eb061d3fe55974b3fe5363
roozbehsanaei <roozbeh.sanaei2@gmail.com>
2017-06-25 18:34:29 +0700

    / ‘examples/java/JNI/simple/HelloJNI.java’

--- examples/java/JNI/simple/HelloJNI.java
+++ examples/java/JNI/simple/HelloJNI.java
@@ -1,6 +1,6 @@
 public class HelloJNI {
    static {
-      System.loadLibrary("hello"); // Load native library at runtime
+      System.load("/home/roozbeh/SparkleShare/SparkleTest/examples/java/JNI/simple/hello.so"); // Load native library at runtime
                                    // hello.dll (Windows) or libhello.so (Unixes)
    }
  
