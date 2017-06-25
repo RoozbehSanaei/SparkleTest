4ad378c8a49edf2565425c9ff4882dba39a10f00
roozbehsanaei <roozbeh.sanaei2@gmail.com>
2017-06-25 19:22:24 +0700

    / ‘examples/java/JNI/simple/test.sh’

--- examples/java/JNI/simple/test.sh
+++ examples/java/JNI/simple/test.sh
@@ -4,6 +4,6 @@ jni_path=/usr/lib/jvm/java-8-oracle/include
 jni_md_path=/usr/lib/jvm/java-8-oracle/include/linux
 javac build/HelloJNI.java
 javah build/HelloJNI
-gcc -I $jni_path -I $jni_md_path HelloJNI.c -shared -o hello.so
+gcc -I $jni_path -I $jni_md_path HelloJNI.c -shared -o build/hello.so
 java HelloJNI
 rm -rf build
